f796eb916bff580bb7f7feed261657ac4a8f4728
wigeys <125973227+wigeys@users.noreply.github.com>
2023-05-25 00:37:12 +0300

    Add files via upload

---
+++ pr14/2121.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main() {
+    try {
+        int dividend, divisor, quotient;
+
+        std::cout << "Enter dividend: ";
+        std::cin >> dividend;
+
+        std::cout << "Enter divisor: ";
+        std::cin >> divisor;
+
+        if (divisor == 0) {
+            throw "Division by zero is not allowed.";
+        }
+
+        quotient = dividend / divisor;
+
+        std::cout << "Quotient: " << quotient << std::endl;
+    }
+    catch (const char* errorMessage) {
+        std::cout << "Error: " << errorMessage << std::endl;
+    }
+
+    return 0;
+}
